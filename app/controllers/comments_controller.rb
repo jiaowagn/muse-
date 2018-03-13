ccda5ed63b0ebc0f81f531d371e82816869cd02f
jiaowagn <wangjiaohere@gmail.com>
2018-03-13 17:14:56 +0800

    add comment to post

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,20 @@
+class CommentsController < ApplicationController
+  before_action :authenticate_user!
+  def create
+    @post = Post.find(params[:post_id])
+    @comment = Comment.new(comment_params)
+    @comment.post = @post
+    @comment.user = current_user
+    if @comment.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
